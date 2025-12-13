3a8c0845f89bc262749105fc9eced032ba0ac16d
Deploy from CI <>
2025-12-13 20:33:52 +0800

    deploy new book

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-6736ff1e.js',
+                path_to_root + 'searchindex-0b69dcd5.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
